1832c4d20557257d27e210a95206d17434bdd7c2
sairam <sairam@DESKTOP-NCOMNEP>
2022-06-15 15:22:34 +0530

    By Monika

---
+++ D54_Git/src/D1_Git/P1_Git.java
@@ -0,0 +1,17 @@
+package D1_Git;
+
+public class P1_Git 
+{
+
+	public void M1()
+	{
+		System.out.println("Hello---Github");
+	}
+	
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("Hey, GitHub..!");
+	}
+
+}
